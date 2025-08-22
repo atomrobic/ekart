173d5989c41b220b891fc88ce1b14b978de27f28
atomrobic <77220996+atomrobic@users.noreply.github.com>
2025-08-22 14:31:29 +0530

    new

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/home" element={<StyleHub />} />
-<Route path="/seller/products/:productId" element={<Description />} />
-        <Route path="/login" element={<Login />} />
+<Route path="/seller/products/:id" element={<Description />} />     
+   <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<LandingPage />} /> {/* Default route */}
 
